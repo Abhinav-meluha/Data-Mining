c4fe98c1ac9133c93b96e16aded04295a552209e
Abhinav-meluha <abhinavchanakya77@gmail.com>
2025-03-21 16:04:45 +0530

    App.py

--- App.py
+++ App.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-
+import matplotlib.pyplot as plt
 import seaborn as sns
 from sklearn.preprocessing import StandardScaler, LabelEncoder
 from sklearn.metrics.pairwise import euclidean_distances, cosine_similarity
